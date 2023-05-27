dd22b6d8d76b640b4897520523f2ef69939c5a4a
Grigoriy Tolstikov <tolstikov.gn@edu.spbstu.ru>
2023-05-27 21:40:17 +0300

    remove check login

--- python_http/http_server.py
+++ python_http/http_server.py
@@ -182,13 +182,13 @@ class HTTPServer(TCPServer):
                            user.encode("utf-8")
                 return self.http_response(data=data, status_code=200, uri=uri, method_type=method_type, addr=addr)
         else:
-            if uri in Settings.login_resources:
-                res, (user, role) = self.check_auth(headers, "GET", uri, addr=addr)
-                if res:
-                    data = b"You are not authorized with this role!"
-                else:
-                    data = b"You authorized as user: " + user.encode("utf-8") + b" with role: " + role.encode("utf-8")
-                return self.http_response(data=data, status_code=200, uri=uri, method_type=method_type, addr=addr)
+            # if uri in Settings.login_resources:
+            #     res, (user, role) = self.check_auth(headers, "GET", uri, addr=addr)
+            #     if res:
+            #         data = b"You are not authorized with this role!"
+            #     else:
+            #         data = b"You authorized as user: " + user.encode("utf-8") + b" with role: " + role.encode("utf-8")
+            #     return self.http_response(data=data, status_code=200, uri=uri, method_type=method_type, addr=addr)
             try:
                 with open(self.root_package + uri, "rb") as f:
                     data = f.read()
